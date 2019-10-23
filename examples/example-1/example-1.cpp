5aa92876e9ca8a74b55088202ce7682faabc318e
Perez <mperez4@saic.edu>
2019-10-23 12:38:41 -0500

    external led working.

--- examples/example-1/example-1.cpp
+++ examples/example-1/example-1.cpp
@@ -1,15 +1,27 @@
 #include "Cubeio.h"
+// #define PIXEL_COUNT 1
+// #define PIXEL_PIN D1
+// #define PIXEL_TYPE WS2812B
 
-Cubeio mycube(1,2,3,4);
+//Adafruit_NeoPixel externalLed = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
+
+Cubeio mycube(D6, D1, A1,A2,A3);
 char active_side;
 
 void setup(){
   Serial.begin(9600);
+  mycube.setThreshold(30);
+  mycube.enableLedControl();
 }
 
 void loop(){
   active_side = mycube.getActiveSide();
+  mycube.setLedColor(255,0,0);
   Serial.println(active_side);
 
-  delay(2000);
+  delay(1000);
+  mycube.setLedColor(0,255,0);
+  delay(1000);
+  mycube.setLedColor(0,0,255);
+  delay(1000);
 }
